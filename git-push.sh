329eefdff636d790ab0e49d69d04e3838ec716a1
SirFudge <51856100+SirFudge@users.noreply.github.com>
2021-10-14 13:46:35 +0200

    Update git-push.sh

--- git-push.sh
+++ git-push.sh
@@ -1,9 +1,13 @@
+#Starting script.
+
 echo "This script automatically pushes your repo to Github!"
 
 echo "You are currently in the branch:"
 git branch
 
 sleep 3
+
+#Choice of running  the script.
 
 echo "Do you want to run the script?"
 
@@ -21,12 +25,22 @@ exit 0
 fi
 done
 
+#Commiting.
+
 echo "Commiting your changes"
 echo "What dit you change?"
 read changed
 git commit -m "$changed"
 
+#Pushing.
+
 echo "Pushing your branch"
 echo "What branch where you working in?"
 git push origin $branch
 
+#Finishing script.
+
+echo "Finished Pushing the branch to Github.com"
+echo "Exiting the script."
+
+exit 0
