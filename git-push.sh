9cb68c7342e7c2db5a49338a078ad3b6636d2afc
SirFudge <51856100+SirFudge@users.noreply.github.com>
2021-10-14 13:25:14 +0200

    Update git-push.sh

--- git-push.sh
+++ git-push.sh
@@ -1,1 +1,26 @@
+echo "This script automatically pushes your repo to Github!"
+
+echo "You are currently in the branch:"
+git branch
+
+sleep 3
+
+echo "Do you want to run the script?"
+
+read anwser
+if  [[ "$anwser" == 'yes' ]] || [[ "$anwser" == 'Yes' ]]
+then
+
+echo "Staring to push this branch."
+
+else
+then
+echo "Stopping the script!"
+exit 0
+
+fi
+done
+
+git commit
+git push origin
 
